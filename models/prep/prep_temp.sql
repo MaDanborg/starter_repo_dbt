38175672268035779a87deb00745e8cfb1422d4a
Marilia Borges <dborges.ma@gmail.com>
2024-01-17 15:58:52 +0100

    Jan 17 updates

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -2,11 +2,24 @@
     SELECT * 
     FROM {{ref('staging_weather')}}
 ),
-add_weekday AS (
+add_timestamps AS (
     SELECT *,
         DATE_PART('dow', date) AS weekday,
-        DATE_PART('day', date) AS day_num
+        DATE_PART('day', date) AS day
+        DATE_PART('week', date) AS week
+        DATE_PART('month', date) AS month
+        DATE_PART('quarter', date) AS quarter
     FROM temperature_daily
 )
 SELECT *
-FROM add_weekday
+FROM add_timestamps
+
+add_averages AS (
+    select avgtemp_c
+    , week
+    , avg(avgtemp_c) as weekly_averages
+    from temperature_daily
+    group by week
+)
+SELECT *
+FROM add_averages
