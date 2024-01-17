c020b70fdbcae3fbb5d4bd9b0217aa4cd24494e2
Marilia Borges <dborges.ma@gmail.com>
2024-01-17 17:17:50 +0100

    jan 17 updates

--- models/prep/prep_temp2.sql
+++ models/prep/prep_temp2.sql
@@ -3,8 +3,7 @@ WITH temperature_daily AS (
     FROM {{ref('staging_weather')}}
 ),
 add_averages AS (
-    SELECT *
-        , DATE_PART('week', date) AS week
+    SELECT DATE_PART('week', date) AS week
         , MAX(avgtemp_c) AS weekly_max
         , MIN(avgtemp_c) AS weekly_min
         , AVG(avgtemp_c) AS weekly_avg
