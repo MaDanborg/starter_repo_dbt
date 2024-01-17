0ff8feb6493bc9eaa9f6b8757428915488e6267b
Marilia Borges <dborges.ma@gmail.com>
2024-01-17 16:00:55 +0100

    Jan 17 updates

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -5,9 +5,9 @@
 add_timestamps AS (
     SELECT *,
         DATE_PART('dow', date) AS weekday,
-        DATE_PART('day', date) AS day
-        DATE_PART('week', date) AS week
-        DATE_PART('month', date) AS month
+        DATE_PART('day', date) AS day,
+        DATE_PART('week', date) AS week,
+        DATE_PART('month', date) AS month,
         DATE_PART('quarter', date) AS quarter
     FROM temperature_daily
 )
