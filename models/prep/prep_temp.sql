4d343f7be33032762f9be5301d6fead7a9989f1e
Marilia Borges <dborges.ma@gmail.com>
2024-01-17 16:32:17 +0100

    Jan 17 updates

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -2,24 +2,23 @@
     SELECT * 
     FROM {{ref('staging_weather')}}
 ),
-add_timestamps AS (
-    SELECT *,
-        DATE_PART('dow', date) AS weekday,
-        DATE_PART('day', date) AS day,
-        DATE_PART('week', date) AS week,
-        DATE_PART('month', date) AS month,
-        DATE_PART('quarter', date) AS quarter
-    FROM temperature_daily
-)
-SELECT *
-FROM add_timestamps
-
 SELECT week
     , MAX(avgtemp_c) AS weekly_max
     , MIN(avgtemp_c) AS weekly_min
     , AVG(avgtemp_c) AS weekly_avg
 FROM temperature_daily
-GROUP BY week
-;
+GROUP BY week,
+add_timestamps AS (
+    SELECT *
+        , DATE_PART('dow', date) AS weekday
+        , DATE_PART('day', date) AS day
+        , DATE_PART('week', date) AS week
+        , DATE_PART('month', date) AS month
+        , DATE_PART('quarter', date) AS quarter
+    FROM temperature_daily
+)
+SELECT *
+FROM add_timestamps
+
 
 
