83d8f14c3f98f1ac73eef7ef1bd8e99aba277eab
Marilia Borges <dborges.ma@gmail.com>
2024-01-17 16:10:31 +0100

    Jan 17 updates

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -17,17 +17,6 @@ FROM add_timestamps
 SELECT *
 FROM temperature_daily AS temp
 	SELECT 	week 
-					, AVG(avgtemp_c) AS weekly_averages
-					FROM temperature_daily
-					GROUP BY week, city
-
-
-add_averages AS (
-    select avgtemp_c
-    , week
-    , avg(avgtemp_c) as weekly_averages
-    from temperature_daily
-    group by week
-)
-SELECT *
-FROM add_averages
+			, AVG(avgtemp_c) AS weekly_averages
+			FROM temperature_daily
+			GROUP BY week, city
