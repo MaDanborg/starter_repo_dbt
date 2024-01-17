5723e8b68fd94641d0ee9854a87fd6b03a039be2
Marilia Borges <dborges.ma@gmail.com>
2024-01-17 17:12:01 +0100

    jan 17 updates

--- models/prep/prep_temp2.sql
+++ models/prep/prep_temp2.sql
@@ -4,16 +4,12 @@ WITH temperature_daily AS (
 ),
 add_averages AS (
     SELECT *
-        , DATE_PART('dow', date) AS weekday
-        , DATE_PART('day', date) AS day
         , DATE_PART('week', date) AS week
-        , DATE_PART('month', date) AS month
-        , DATE_PART('quarter', date) AS quarter
         , MAX(avgtemp_c) AS weekly_max
         , MIN(avgtemp_c) AS weekly_min
         , AVG(avgtemp_c) AS weekly_avg
     FROM temperature_daily
-    group by DATE_PART('week', date) AS week
+    group by week
 )
 SELECT *
 FROM add_averages
