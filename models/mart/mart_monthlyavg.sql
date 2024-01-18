c3d28401fb0147309f32bb1f9ffe2906b565cf9c
Marilia Borges <dborges.ma@gmail.com>
2024-01-18 11:20:47 +0100

    Jan 18 updates

--- models/mart/mart_monthlyavg.sql
+++ models/mart/mart_monthlyavg.sql
@@ -2,13 +2,12 @@ WITH total_avg_monthly AS (
         SELECT city
         , country
         , month
-        , week
         , quarter
         , MAX(avgtemp_c) AS max_avg
         , MIN(avgtemp_c) AS min_avg
-        , AVG(avgtemp_c) AS weekly_avg
+        , AVG(avgtemp_c) AS monthly_avg
     FROM {{ref("prep_temp")}}
-    group by month, city, country, week, quarter
+    group by month, city, country, quarter
     order by month
 )
 SELECT * from total_avg_monthly
