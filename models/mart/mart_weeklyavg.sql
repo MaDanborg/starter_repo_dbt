162c7ac94bd1c47259a94e4ca8d2e765d61965f2
Marilia Borges <dborges.ma@gmail.com>
2024-01-18 11:03:54 +0100

    Jan 18 updates

--- models/mart/mart_weeklyavg.sql
+++ models/mart/mart_weeklyavg.sql
@@ -1,9 +1,9 @@
 WITH total_avg AS (
         SELECT city
         , country
-        , DATE_PART('week', date) AS week
-        , DATE_PART('month', date) AS month
-        , DATE_PART('quarter', date) AS quarter
+        , week
+        , month
+        , quarter
         , MAX(avgtemp_c) AS max_avg
         , MIN(avgtemp_c) AS min_avg
         , AVG(avgtemp_c) AS weekly_avg
